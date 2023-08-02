d7f629dc9201cc4d37c227b12baa8fbf0b5a962b
github-repos <sheshankprakash260@gmail.com>
2023-08-03 00:09:58 +0530

    Server creation by terraform for roboshop

--- roboshop_tf_v1/roboshop.tf
+++ roboshop_tf_v1/roboshop.tf
@@ -1,22 +1,42 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "frontend"
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z06631553SNW6N7HBTCKR"
+  name    = "frontend.pdevopst74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "mongodb"
   }
 }
 
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z06631553SNW6N7HBTCKR"
+  name    = "mongodb.pdevopst74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "catalogue"
   }
@@ -26,6 +46,7 @@ resource "aws_instance" "catalogue" {
 resource "aws_instance" "redis" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "redis"
   }
@@ -34,6 +55,7 @@ resource "aws_instance" "redis" {
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "cart"
   }
@@ -43,6 +65,7 @@ resource "aws_instance" "cart" {
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "user"
   }
@@ -53,6 +76,7 @@ resource "aws_instance" "user" {
 resource "aws_instance" "mysql" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "mysql"
   }
@@ -61,6 +85,7 @@ resource "aws_instance" "mysql" {
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "shipping"
   }
@@ -69,6 +94,7 @@ resource "aws_instance" "shipping" {
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "rabbitmq"
   }
@@ -78,6 +104,7 @@ resource "aws_instance" "rabbitmq" {
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
   tags = {
     Name = "payment"
   }

b4b903fc55e2c88ec4bc1088e6e750f819d302d1
github-repos <sheshankprakash260@gmail.com>
2023-08-04 23:16:24 +0530

    Variable in tf

--- 03-variables/sample.tf
+++ 03-variables/sample.tf
@@ -1,9 +1,21 @@
+
 variable "fruits" {
   default = [ "Apple", "Banana"]
-
 }
 
 output "fruits"{
   value= var.fruits
 }
+
+variable "fruits with stock" {
+  default = {
+    apple=100
+    banana=200
+  }
+}
+
+output "fruits with stock"{
+  value= var.fruits["apple"]
+}
+
 

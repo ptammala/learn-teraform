57d5229e8a3fac5621eb521b0e07b9a949065a01
github-repos <sheshankprakash260@gmail.com>
2023-08-06 00:29:01 +0530

    componets

--- roboshop_v2_tf/main.tf
+++ roboshop_v2_tf/main.tf
@@ -35,11 +35,14 @@ resource "aws_instance" "instance" {
     Name = lookup(each.value,"name",null )
   }
 }
-#
-#resource "aws_route53_record" "frontend" {
-#  zone_id = var.zone_id
-#  name    = "frontend.pdevopst74.online"
-#  type    = "A"
-#  ttl     = 30
+
+resource "aws_route53_record" "record" {
+  for_each = var.components
+
+  zone_id = var.zone_id
+  name    = "${lookup(each.value,"name",null)}.pdevopst74.online"
+  type    = "A"
+  ttl     = 30
+  records = lookup(lookup(aws_instance.instance,each.key,null),private_ip,null)
 #  records = [aws_instance.frontend.private_ip]
-#}
+}

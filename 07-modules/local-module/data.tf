c1ec46d7e642343293785cdd7489ec8cb4681dfd
github-repos <sheshankprakash260@gmail.com>
2023-08-09 19:56:45 +0530

    componets

--- 07-modules/local-module/data.tf
+++ 07-modules/local-module/data.tf
@@ -1,7 +1,12 @@
 data "aws_ami_ids" "ami" {
   owners = ["973714476881"]
+  stock = "Prakash"
 }
 
 output "ami"{
   value = data.aws_ami_ids.ami
+}
+
+output "ami_C"{
+  value = data.aws_ami_ids.ami.stock
 }
--- 07-modules/main.tf
+++ 07-modules/main.tf
@@ -1,3 +1,11 @@
 module "test" {
   source = "./local-module"
 }
+
+output "test" {
+  value = module.test
+}
+
+output "test1" {
+  value = module.test.ami_C
+}

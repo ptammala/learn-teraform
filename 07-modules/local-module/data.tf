54e5ca6b957b6222c967f33396b1426a20ca34bf
github-repos <sheshankprakash260@gmail.com>
2023-08-09 23:01:12 +0530

    componets

--- 07-modules/local-module/data.tf
+++ 07-modules/local-module/data.tf
@@ -1,13 +1,16 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
+  most_recent=true
   owners = ["973714476881"]
-  name_regex = "Cento*"
+  name_regex = "Centos-8-DevOps-Practice"
 
 }
 
 output "ami"{
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
 
-output "ami_C"{
-  value = data.aws_ami_ids.ami.name_regex
+resource "aws_instance" "instance" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sgr-0f8363e4beadb8bcd" ]
 }

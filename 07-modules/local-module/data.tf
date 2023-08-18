4766e4d95d9511b4f5c857eadb3b160a9b024c4b
github-repos <sheshankprakash260@gmail.com>
2023-08-19 04:49:29 +0530

    componets

--- 07-modules/local-module/data.tf
+++ 07-modules/local-module/data.tf
@@ -8,9 +8,9 @@ data "aws_ami" "ami" {
 output "ami"{
   value = data.aws_ami.ami
 }
-
-resource "aws_instance" "instance" {
-  ami = data.aws_ami.ami.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ "sgr-0f8363e4beadb8bcd" ]
-}
+#
+#resource "aws_instance" "instance" {
+#  ami = data.aws_ami.ami.id
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [ "sgr-0f8363e4beadb8bcd" ]
+#}

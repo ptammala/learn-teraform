366684c620157cb5ea51d1691cdad6291a5a403a
github-repos <sheshankprakash260@gmail.com>
2023-08-19 20:04:45 +0530

    componets

--- 05-data_source/main.tf
+++ 05-data_source/main.tf
@@ -1,10 +1,9 @@
 
-data "aws_ami" "ami" {
-
+data "aws_ami_ids" "Centos" {
   name_regex = "Centos-*"
   owners     = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami.ami
+  value = data.aws_ami_ids.Centos
 }

957677b8dbdb70d8b5596369b81b6aea775ec9e7
github-repos <sheshankprakash260@gmail.com>
2023-08-19 21:42:18 +0530

    componets

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -20,6 +20,6 @@ resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
   tags = {
-    Name = "HelloWorld"
+    Name = element(var.components,10 )
   }
 }

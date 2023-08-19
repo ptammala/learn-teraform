da0c6d9e66029e3c19fe7d1a31c772a9b33118a8
github-repos <sheshankprakash260@gmail.com>
2023-08-19 21:35:50 +0530

    componets

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -2,16 +2,21 @@ variable "components" {
   default=["frontend","mongodb","catalogue"]
 }
 
+#
+#resource "aws_instance" "instance" {
+#  count = length(var.components)
+#
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
+#  tags = {
+#    Name = element(var.components, count.index)
+#
+##    Name = var.components(count.index)
+#  }
+#}
 
-resource "aws_instance" "instance" {
-  count = length(var.components)
-
+resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
-  tags = {
-    Name = element(var.components, count.index)
-
-#    Name = var.components(count.index)
-  }
 }

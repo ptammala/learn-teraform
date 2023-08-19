545c05528e4b8924a19d6adcb4e23e7568fdc3c1
github-repos <sheshankprakash260@gmail.com>
2023-08-19 23:24:56 +0530

    Using for_each loop

--- 06-loops/for_each/main.tf
+++ 06-loops/for_each/main.tf
@@ -1,15 +1,8 @@
 variable "components"{
-
-  frontend = { name = "frontend"}
-  mongodb = { name = "mongodb" }
-
+  default = { forntend = { name = "frontend"},mongodb = { name = "mongodb"} }
 }
 
 resource "aws_security_group" "allow_tls" {
-
   for_each = var.components
-  name = lookup(var.components,each.value["name"],null)
-
-    #    Name = var.components(count.index)
-  }
+  name        = lookup(var.components,each.value["name"],null )
 }

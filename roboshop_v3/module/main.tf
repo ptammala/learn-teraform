f2d84df3f33175abbac871f351a8bcfaab8db121
github-repos <sheshankprakash260@gmail.com>
2023-08-21 00:22:55 +0530

    Using for_each loop

--- roboshop_v3/module/main.tf
+++ roboshop_v3/module/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "instance" {
   ami                        = data.aws_ami.ami.id
   instance_type              = var.instant_type
   vpc_security_group_ids     = var.security_group
+  tags                       = var.name
 }
 
 

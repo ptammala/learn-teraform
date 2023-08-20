7b4458a76bacbc0e48d6a0165c13a31e01fdd516
github-repos <sheshankprakash260@gmail.com>
2023-08-20 23:09:31 +0530

    Using for_each loop

--- 07-modules/local-module/data.tf
+++ 07-modules/local-module/data.tf
@@ -9,6 +9,11 @@ output "ami"{
   value = data.aws_ami.ami
 }
 
+output "ami2"{
+  value = data.aws_ami.ami.owners
+}
+
+
 #resource "aws_instance" "instance" {
 #  ami = data.aws_ami.ami.id
 #  instance_type = "t2.micro"

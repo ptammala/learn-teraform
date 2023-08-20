141140cfe7fa3ff7097ec4978966a06aae70f07e
github-repos <sheshankprakash260@gmail.com>
2023-08-20 23:22:56 +0530

    Using for_each loop

--- 07-modules/local-module/data.tf
+++ 07-modules/local-module/data.tf
@@ -15,7 +15,8 @@ output "ami2"{
 
 
 resource "aws_instance" "instance" {
+
   ami = data.aws_ami.ami.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ "sgr-0f8363e4beadb8bcd" ]
+  vpc_security_group_ids = [ "sg-0b92db49845820fb2" ]
 }

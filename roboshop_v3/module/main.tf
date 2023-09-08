9508fd0a4dfc0bf1d960c935462f14639c003320
github-repos <sheshankprakash260@gmail.com>
2023-09-08 08:20:12 +0530

    Variables

--- roboshop_v3/module/main.tf
+++ roboshop_v3/module/main.tf
@@ -21,11 +21,12 @@ resource "aws_route53_record" "record" {
 resource "null_resource" "ansible" {
   depends_on = [ aws_route53_record.record ]
   provisioner "local-exec" {
+
     command = <<EOF
 cd /home/centos/roboshop-ansible
 git pull
-sleep 15
-ansible-playbook -i "${var.name}.pdevopst74.online", main.yml -e ansible_user=centos ansible_password=DevOps321 -e component="${var.name}"
+sleep 30
+ansible-playbook -i ${var.name}.pdevopst74.online, main.yml -e ansible_user=centos ansible_password=DevOps321 -e component=${var.name}
 EOF
   }
 

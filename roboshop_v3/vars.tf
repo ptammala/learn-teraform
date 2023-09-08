b7d9fa1883a93c0e28f7c418054da0a97f1cc3e2
github-repos <sheshankprakash260@gmail.com>
2023-09-08 20:58:47 +0530

    Variables

--- roboshop_v3/vars.tf
+++ roboshop_v3/vars.tf
@@ -10,44 +10,44 @@ variable "component" {
   default = {
     frontend = {
       name = "frontend"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     cart = {
       name = "cart"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     mongodb = {
       name = "mongodb"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
 
     catalogue = {
       name = "catalogue"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     user = {
       name = "user"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     redis = {
       name = "redis"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     mysql = {
       name = "mysql"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     shipping = {
       name = "shipping"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     payment = {
       name = "payment"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
     rabbitmq = {
       name = "rabbitmq"
-      instant_type = "t2.micro"
+      instant_type = "t3.micro"
     }
 
 
